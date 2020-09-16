df93eaed435e4bec9ad020c0b4dbff8214d7bdd4
matthewlazewski <matthew.lazewski@gmail.com>
2020-09-16 09:31:16 -0500

    Done.

--- src/reducers/manageTodo.js
+++ src/reducers/manageTodo.js
@@ -1,8 +1,15 @@
+import { connect } from "react-redux";
+
 export default function manageTodo(state = {
   todos: [],
 }, action) {
+  switch(action.type){
+    case 'ADD_TODO':
+      console.log({todos: state.todos.concat(action.payload.text) });
 
-  console.log("reducer received this action:", action);
+      return { todos: state.todos.concat(action.payload.text) };
 
-  return state;
+    default:
+      return state;
+  }
 }
